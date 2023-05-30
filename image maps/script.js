2ce257888fdfc688d104eabb99c385d3d2cb5d48
Victoria Martinez <martinezvictoria781@gmail.com>
2023-05-30 15:59:04 +0100

    practice

---
+++ image maps/script.js
@@ -0,0 +1,36 @@
+const painting = document.getElementById('painting').addEventListener('click', function(){
+    alert('painting');
+});
+
+const clock = document.getElementById('open');
+const modale = document.getElementById('demoModal');
+
+clock.addEventListener('click', function(){
+    modale.showModal();
+})
+
+
+const updateButton = document.getElementById("updateDetails");
+const cancelButton = document.getElementById("cancel");
+const dialog = document.getElementById("favDialog");
+dialog.returnValue = "favAnimal";
+
+function openCheck(dialog) {
+  if (dialog.open) {
+    console.log("Dialog open");
+  } else {
+    console.log("Dialog closed");
+  }
+}
+
+// Update button opens a modal dialog
+updateButton.addEventListener("click", () => {
+  dialog.showModal();
+  openCheck(dialog);
+});
+
+// Form cancel button closes the dialog box
+cancelButton.addEventListener("click", () => {
+  dialog.close("animalNotChosen");
+  openCheck(dialog);
+});
